11baf14b6d74b91b8c787d7e028b14679607db22
pg147 <xyzprathamesh147@gmail.com>
2025-01-28 00:23:15 +0530

    Blank routes created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,24 @@
+// Dependencies
 import express from "express";
 import dotenv from "dotenv";
 
+// Routes
+import { userRoutes, authRoutes, adminRoutes, albumRoutes, songRoutes, statRoutes } from "./routes";
+
+// .env config
 dotenv.config({
     path: '.env.local'
 });
 
-const app = express();
-const PORT = process.env.PORT;
+const app = express();  // express app
+const PORT = process.env.PORT;   // defined PORT
+
+app.use('/api/v1/users', userRoutes); 
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/admin', adminRoutes);
+app.use('/api/v1/songs', songRoutes);
+app.use('/api/v1/albums', albumRoutes);
+app.use('/api/v1/stats', statRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started at : `, PORT);
---
+++ backend/src/routes/index.js
@@ -0,0 +1,8 @@
+import userRoutes from "./user.route.js";
+import authRoutes from "./auth.route.js";
+import adminRoutes from "./admin.route.js";
+import songRoutes from "./song.route.js";
+import albumRoutes from "./album.route.js";
+import statRoutes from "./stat.route.js";
+
+export { userRoutes, adminRoutes, authRoutes, songRoutes, albumRoutes, statRoutes };
