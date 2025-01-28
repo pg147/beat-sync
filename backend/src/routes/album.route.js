052c177681b13cf5c7e69458ae49cfa10b51aded
pg147 <xyzprathamesh147@gmail.com>
2025-01-28 23:32:01 +0530

    Routes updated for album

--- backend/src/routes/album.route.js
+++ backend/src/routes/album.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { getAlbum, getAllAlbums } from "../controllers/album.controller.js";
 
 const router = Router();
+
+router.get('/albums', getAllAlbums);
+router.get('/album/:id', getAlbum);
 
 export default router;
