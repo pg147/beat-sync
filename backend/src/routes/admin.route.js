efbaab020ed590d0791df8e0aedf9abed4bfb028
pg147 <xyzprathamesh147@gmail.com>
2025-01-28 20:00:28 +0530

    Route updated for create song

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,5 +1,24 @@
 import { Router } from "express";
 
-const router = Router();
+// Controllers
+import { createSong } from "../controllers/admin.controller.js";
+
+// Middlewares
+import upload from "../middlewares/multer.middleware.js";
+import { protectRoute, requireAdmin } from "../middlewares/auth.middleware";
+
+const router = Router();  // admin router
+
+// Secure Routes
+router.post('/song/add', protectRoute, requireAdmin, upload.fields([
+    {
+        name: 'audio',
+        maxCount: 1
+    },
+    {
+        name: 'coverImage',
+        maxCount: 1
+    }
+]), createSong);  // for creating a song
 
 export default router;
