baeb684287ba893058b67a8dea3c5ca73d3e62ad
pg147 <xyzprathamesh147@gmail.com>
2025-01-28 23:17:05 +0530

    Admin routes updated and middleware check optimized

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 // Controllers
-import { createSong } from "../controllers/admin.controller.js";
+import { checkAdmin, createAlbum, createSong, deleteAlbum, deleteSong } from "../controllers/admin.controller.js";
 
 // Middlewares
 import upload from "../middlewares/multer.middleware.js";
@@ -9,8 +9,13 @@ import { protectRoute, requireAdmin } from "../middlewares/auth.middleware";
 
 const router = Router();  // admin router
 
-// Secure Routes
-router.post('/song/add', protectRoute, requireAdmin, upload.fields([
+// Shared middleware that would be followed by each route to check auth and admin status
+router.use(protectRoute, requireAdmin);  
+
+// Routes
+router.get('/check', checkAdmin);  // for returning a boolean value 
+
+router.post('/songs/add', upload.fields([
     {
         name: 'audio',
         maxCount: 1
@@ -20,5 +25,9 @@ router.post('/song/add', protectRoute, requireAdmin, upload.fields([
         maxCount: 1
     }
 ]), createSong);  // for creating a song
+router.delete('/song/delete/:id', deleteSong);  // for deleting a song
+
+router.post('/albums/add', createAlbum);  // for creating an album
+router.delete('/album/delete/:id', deleteAlbum);  // for deleting an album
 
 export default router;
