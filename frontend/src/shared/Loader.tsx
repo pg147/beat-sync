71033e6519b1febf2d4e3ebea8a5955be1c11695
pg147 <xyzprathamesh147@gmail.com>
2025-01-30 01:08:55 +0530

    Styling improvements for Brand component

--- frontend/src/shared/Loader.tsx
+++ frontend/src/shared/Loader.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from "lucide-react";
 export default function Loader() {
   return (
     <div className="h-screen w-full flex justify-center items-center pointer-events-none">
-        <Loader2 className="size-8 md:size-10 animate-spin" />
+        <Loader2 className="size-8 md:size-10 animate-spin text-primary" />
     </div>
   )
 }
