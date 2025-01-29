8461907fdea18868733d9d2ac022668a72104e6f
pg147 <xyzprathamesh147@gmail.com>
2025-01-30 01:09:11 +0530

    Styling improvements for Brand component

---
+++ frontend/src/shared/Brand.tsx
@@ -0,0 +1,9 @@
+
+export default function Brand() {
+  return (
+    <div className="size-fit flex items-center gap-x-2 p-4">
+        <img src="/logo.png" alt="logo" className="size-8" />
+        <h1 className="text-lg md:text-xl font-semibold">Beat<span className="text-primary">.sync</span></h1>
+    </div>
+  )
+}
