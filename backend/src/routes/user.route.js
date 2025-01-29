014c9c4b293d55a484cb57c00df6c19ce2e5398f
pg147 <xyzprathamesh147@gmail.com>
2025-01-29 20:14:27 +0530

    User route updated

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
 
+// Controllers
+import { signup } from "../controllers/auth.controller.js";
+import { getAllUsers } from "../controllers/users.controller.js";
+
+// Middlewares
+import { protectRoute } from "../middlewares/auth.middleware.js";
+
 const router = Router();
+
+router.post('/callback', signup);
+router.get('/users', protectRoute, getAllUsers);
 
 export default router;
