dc23a3a07357cdc1f925fe18b8a5aec74c51a356
pg147 <xyzprathamesh147@gmail.com>
2025-01-29 20:31:41 +0530

    Stats route updated

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
 
+// Controllers
+import { getAllStats } from "../controllers/stats.controller.js";
+
+// Middlewares
+import { protectRoute, requireAdmin } from "../middlewares/auth.middleware.js"
+
 const router = Router();
+
+router.get("/stats", protectRoute, requireAdmin, getAllStats);
 
 export default router;
