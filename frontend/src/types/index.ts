bda60eab45b54a1731158aae1f0c825539a83e6b
pg147 <xyzprathamesh147@gmail.com>
2025-01-30 23:39:59 +0530

    Types updated

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,28 +1,31 @@
 export interface Song {
-    _id: string
-    title: string
-    artist: string
-    albumId: string
-    audioURL: string
-    coverImageURL: string
-    duration: number
-    createdAt: Date
-    updatedAt: Date
+  _id: string;
+  title: string;
+  artist: string;
+  albumId: string;
+  audioURL: string;
+  coverImageURL: string;
+  duration: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface Album {
-    _id: string
-    title: string
-    artist: string
-    coverImageURL: string
-    releaseYear: Date
-    songs: Array<Song>
+  _id: string;
+  title: string;
+  artist: string;
+  coverImageURL: string;
+  releaseYear: Date;
+  songs: Array<Song>;
 }
 
 export interface MusicStore {
-    songs: Array<Song>;
-    albums: Array<Album>;
-    isLoading: boolean;
-    error: string | null;
-    fetchAlbums: () => Promise<void>;
+  songs: Array<Song>;
+  albums: Array<Album>;
+  isLoading: boolean;
+  isAlbumLoading: boolean;
+  error: string | null;
+  currentAlbum: object | null;
+  fetchAlbums: () => Promise<void>;
+  fetchCurrentAlbum: (albumId: string | undefined) => Promise<void>;
 }
