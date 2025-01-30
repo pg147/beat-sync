b99e8d89085544fbb8570a40aaa4b4df7b16060d
pg147 <xyzprathamesh147@gmail.com>
2025-01-30 23:40:24 +0530

    Index imports updated

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,5 +1,6 @@
 import Homepage from "./homepage/Homepage";
 import AuthCallback from "./auth-callback/AuthCallback";
 import Messages from "./messages/Messages";
+import Album from "./album/Album";
 
-export { Homepage, AuthCallback, Messages };
+export { Homepage, AuthCallback, Messages, Album };
