be17b85d7d4280b4177ed98b38edfea69160fd0e
pg147 <xyzprathamesh147@gmail.com>
2025-01-30 16:21:33 +0530

    MainLayout Route added for dynamic changing components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthCallback, Homepage } from "./pages";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Homepage />} />
         <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />} />
         <Route path="/auth-callback" element={<AuthCallback />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Homepage />} />
+        </Route>
       </Routes>
     </>
   )
