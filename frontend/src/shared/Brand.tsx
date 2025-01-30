1f0f0cc097be3b3691d8c101d44ec9d9552f3662
pg147 <xyzprathamesh147@gmail.com>
2025-01-30 18:06:25 +0530

    Link added at brand

--- frontend/src/shared/Brand.tsx
+++ frontend/src/shared/Brand.tsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 
 export default function Brand() {
   return (
-    <div className="size-fit flex items-center gap-x-2 p-4">
+    <Link to={'/'} className="size-fit flex items-center gap-x-2 p-4">
         <img src="/logo.png" alt="logo" className="size-8" />
         <h1 className="text-lg md:text-xl font-semibold">Beat<span className="text-primary">.sync</span></h1>
-    </div>
+    </Link>
   )
 }
