dd65e03ec40cac0f5f3181d5522ad7d8f3e0ddcb
pg147 <xyzprathamesh147@gmail.com>
2025-01-30 23:59:22 +0530

    Current Album state added in store

--- frontend/src/store/useMusicStore.ts
+++ frontend/src/store/useMusicStore.ts
@@ -6,14 +6,16 @@ export const useMusicStore = create<MusicStore>((set) => ({
   albums: [],
   songs: [],
   isLoading: false,
+  isAlbumLoading: false,
   error: null,
+  currentAlbum: null,
 
   fetchAlbums: async () => {
     set({ isLoading: true, error: null });
 
     try {
       const response = await axiosInstance.get("/albums");
-      
+
       if (response) {
         set({ albums: response.data.albums });
       }
@@ -21,6 +23,22 @@ export const useMusicStore = create<MusicStore>((set) => ({
       set({ error: error.response.data.message });
     } finally {
       set({ isLoading: false });
+    }
+  },
+
+  fetchCurrentAlbum: async (albumId: string | undefined) => {
+    set({ isAlbumLoading: true, error: null });
+
+    try {
+      const album = await axiosInstance.get(`/albums/${albumId}`);
+
+      if (album) {
+        set({ currentAlbum: album });
+      }
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isAlbumLoading: false });
     }
   },
 }));
