42f68510284f622c6a781b6ed295a4b198df5a56
pg147 <xyzprathamesh147@gmail.com>
2025-01-30 18:06:57 +0530

    indexes updated for components and pages

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,4 +1,5 @@
 import Topbar from "./Topbar";
 import SignInOAuthButton from "./SignInOAuthButton";
+import LeftSideBar from "./LeftSideBar";
 
-export { Topbar, SignInOAuthButton };
+export { Topbar, SignInOAuthButton, LeftSideBar };
---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,5 @@
+import Homepage from "./homepage/Homepage";
+import AuthCallback from "./auth-callback/AuthCallback";
+import Messages from "./messages/Messages";
+
+export { Homepage, AuthCallback, Messages };
