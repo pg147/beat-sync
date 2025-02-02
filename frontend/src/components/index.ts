33eee2060bfefdba4a95e33b4585f18daa5836d0
pg147 <xyzprathamesh147@gmail.com>
2025-02-02 19:01:22 +0530

    Index updated

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,6 @@
 import Topbar from "./Topbar";
 import SignInOAuthButton from "./SignInOAuthButton";
 import LeftSideBar from "./LeftSideBar";
+import FriendsActivity from "./FriendsActivity";
 
-export { Topbar, SignInOAuthButton, LeftSideBar };
+export { Topbar, SignInOAuthButton, LeftSideBar, FriendsActivity };
