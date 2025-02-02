6eaa70f424f353d765034357d70d8e39f01ee3b2
pg147 <xyzprathamesh147@gmail.com>
2025-02-02 19:42:18 +0530

    Avatar Fallback Util Fn added

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -10,3 +10,10 @@ export const formatDuration = (seconds: number) => {
   const remainingSeconds = seconds % 60;
   return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
 };
+
+export const avatarFallback = (name: string) => {
+  const splitName = name.split(" ");
+  const initials = `${splitName[0][0]}${splitName[1][0]}`
+
+  return initials;
+}
