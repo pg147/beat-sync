dd9a7608601f17a607cbfe1edf8c8c8910c3ee35
pg147 <xyzprathamesh147@gmail.com>
2025-02-03 21:45:21 +0530

    Audio Player created - Basic

--- frontend/src/components/AudioPlayer.tsx
+++ frontend/src/components/AudioPlayer.tsx
@@ -9,14 +9,29 @@ export default function AudioPlayer() {
 
     // To handle play and pause
     useEffect(() => {
-        if (isPlaying) {
-            audioRef.current?.play();
-        } else audioRef.current?.pause();
+        const audio = audioRef.current;
+        if (!audio) return;
+
+        const playAudio = async () => {
+            if (isPlaying) {
+                try {
+                    await audio.play();
+                } catch (error: any) {
+                    if (error.name !== "AbortError") {
+                        console.error("Playback error:", error);
+                    }
+                }
+            } else {
+                audio.pause();
+            }
+        }
+
+        playAudio();
     }, [isPlaying]);
 
     // To handle audio end functionality
     useEffect(() => {
-        const audio = audioRef.current;  // fetching audio using ref
+        const audio = audioRef?.current;  // fetching audio using ref
 
         // Event handler
         const handleAudioEnd = () => playNext();
@@ -30,17 +45,17 @@ export default function AudioPlayer() {
 
     useEffect(() => {
         if (!audioRef.current || !currentSong) return;
-        const audio = audioRef.current;
+        const audio = audioRef?.current;
 
         // If source for previous audio doesn't match current audio's url therefore song has changed
-        const isSongChanged = prevAudioRef.current !== currentSong?.audioURL;
+        const isSongChanged = prevAudioRef?.current !== currentSong?.audioURL;
 
         if (isSongChanged) {
-            audio.src = currentSong.audioURL;   // setting current audio's src
+            audio.src = currentSong?.audioURL;   // setting current audio's src
             audio.currentTime = 0;   // resetting the duration
 
             prevAudioRef.current = currentSong?.audioURL;   // setting prevAudioRef with current audio URL
-            if (isPlaying) audio.play();  // finally playing if isPlaying is true
+            if (isPlaying) audio?.play();  // finally playing if isPlaying is true
         }
     }, [currentSong, isPlaying]);
 
