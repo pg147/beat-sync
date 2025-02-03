c98c3777a16574169d09b7f9cc3d814ec6b6ccdb
pg147 <xyzprathamesh147@gmail.com>
2025-02-03 17:16:20 +0530

    Album page functionality added

--- frontend/src/pages/album/Album.tsx
+++ frontend/src/pages/album/Album.tsx
@@ -4,15 +4,20 @@ import { useEffect } from "react";
 // react-router-dom
 import { useParams } from "react-router-dom";
 
-// Global State Store - Music
+// Global State Stores
 import { useMusicStore } from "@/store/useMusicStore"
+import { usePlayerStore } from "@/store/usePlayerStore";
+
+// Components
+import AudioPlayer from "@/components/AudioPlayer";
 
 // Shadcn components
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
 
 // Icon Library
-import { Play } from "lucide-react";
+import { Pause, Play } from "lucide-react";
+import { MusicNote03Icon } from "hugeicons-react";
 
 // Skeleton
 import AlbumSkeleton from "@/components/skeletons/AlbumSkeleton";
@@ -22,16 +27,32 @@ import { formatDuration } from "@/lib/utils";
 
 export default function Album() {
     const { currentAlbum, fetchCurrentAlbum, isAlbumLoading } = useMusicStore();
+    const { playAlbum, currentSong, isPlaying, togglePlay } = usePlayerStore();
     const { id } = useParams();
 
     useEffect(() => {
         if (id) fetchCurrentAlbum(id);
     }, [id, fetchCurrentAlbum]);
 
+    const handlePlayButton = () => {
+        const isAlbumPlaying = currentAlbum?.songs.some(song => song._id === currentSong?._id);
+
+        // If album is already playing then act as a control button
+        if (isAlbumPlaying) togglePlay();
+        else handlePlaySong(0);  // else start playing album from start
+    }
+
+    const handlePlaySong = (index: number) => {
+        if (!currentAlbum) return;
+
+        playAlbum(currentAlbum?.songs, index);
+    }
+
     if (isAlbumLoading) return <AlbumSkeleton />
 
     return (
         <div className="w-full h-full">
+            <AudioPlayer />
             <ScrollArea className="h-full">
                 <div className="relative min-h-full">
                     {/* Background gradient */}
@@ -48,9 +69,9 @@ export default function Album() {
                                 <p className="font-medium mt-2"><span className="text-primary">{currentAlbum?.artist}</span> • {currentAlbum?.songs.length + " " + "songs"} • {currentAlbum?.releaseYear}</p>
                             </div>
                         </div>
-                        
-                        <Button className="size-fit rounded-full bg-primary lg:hover:bg-primary p-4 mt-9 transition-all duration-300 ease-in-out lg:hover:scale-125">
-                            <Play className="size-7" fill="#FFFFFF" />
+
+                        <Button onClick={handlePlayButton} className={`size-fit rounded-full bg-primary lg:hover:bg-primary p-4 mt-9 transition-all duration-300 ease-in-out ${isPlaying ? '' : 'lg:hover:scale-125'}`}>
+                            {isPlaying ? <Pause className="size-7" fill="#FFFFFF" /> : <Play className="size-7" fill="#FFFFFF" />}
                         </Button>
 
                         <div className="mt-9">
@@ -71,27 +92,33 @@ export default function Album() {
                             </div>
 
                             <div className="w-full space-y-1">
-                                {currentAlbum?.songs.map((song, index) => (
-                                    <div key={song._id} className="cursor-pointer grid grid-cols-12 items-center font-medium group lg:hover:bg-tile/60 px-6 py-3 rounded-xl">
-                                        <div className="col-span-1">
-                                            <p className="block group-hover:hidden">{index + 1}</p>
-                                            <Play className="text-primary size-5 hidden group-hover:block" fill="#EB4D7D"/>
-                                        </div>
-                                        <div className="col-span-7 flex items-center gap-x-4">
-                                            <img src={song.coverImageURL} alt={song.title} className="size-12 aspect-square rounded-md" />
-                                            <div>
-                                                <h1>{song.title}</h1>
-                                                <p className="text-subheading">{song.artist}</p>
+                                {currentAlbum?.songs.map((song, index) => {
+                                    const isCurrentSong = currentSong?._id === song._id;
+                                    
+                                    return (
+                                        <div onClick={() => handlePlaySong(index)} key={song._id} className="cursor-pointer grid grid-cols-12 items-center font-medium group lg:hover:bg-tile/60 px-6 py-3 rounded-xl">
+                                            <div className="col-span-1">
+                                                {isCurrentSong ? <MusicNote03Icon className="size-5 text-primary" /> : <>
+                                                    <p className="block group-hover:hidden">{index + 1}</p>
+                                                    <Play className="text-primary size-5 hidden group-hover:block" fill="#EB4D7D" />
+                                                </>}
+                                            </div>
+                                            <div className="col-span-7 flex items-center gap-x-4">
+                                                <img src={song.coverImageURL} alt={song.title} className="size-12 aspect-square rounded-md" />
+                                                <div>
+                                                    <h1>{song.title}</h1>
+                                                    <p className="text-subheading">{song.artist}</p>
+                                                </div>
+                                            </div>
+                                            <div className="col-span-2">
+                                                <p>{song.createdAt.split("T")[0]}</p>
+                                            </div>
+                                            <div className="col-span-2">
+                                                <p className="text-center">{formatDuration(song.duration)}</p>
                                             </div>
                                         </div>
-                                        <div className="col-span-2">
-                                            <p>{song.createdAt.split("T")[0]}</p>
-                                        </div>
-                                        <div className="col-span-2">
-                                            <p className="text-center">{formatDuration(song.duration)}</p>
-                                        </div>
-                                    </div>
-                                ))}
+                                    )
+                                })}
                             </div>
                         </div>
                     </div>
