aeb0086bfcc594127bd3e7483d5c6062a213680b
pg147 <xyzprathamesh147@gmail.com>
2025-02-03 17:16:41 +0530

    Basic functions added for Audio Player

---
+++ frontend/src/components/AudioPlayer.tsx
@@ -0,0 +1,50 @@
+import { usePlayerStore } from "@/store/usePlayerStore"
+import { useEffect, useRef } from "react";
+
+export default function AudioPlayer() {
+    const audioRef = useRef<HTMLAudioElement>(null);
+    const prevAudioRef = useRef<string | null>(null);
+
+    const { currentSong, isPlaying, playNext } = usePlayerStore();
+
+    // To handle play and pause
+    useEffect(() => {
+        if (isPlaying) {
+            audioRef.current?.play();
+        } else audioRef.current?.pause();
+    }, [isPlaying]);
+
+    // To handle audio end functionality
+    useEffect(() => {
+        const audio = audioRef.current;  // fetching audio using ref
+
+        // Event handler
+        const handleAudioEnd = () => playNext();
+
+        // Listening to end event for an audio
+        audio?.addEventListener("ended", handleAudioEnd);
+
+        // Cleanup
+        return () => audio?.removeEventListener("ended", handleAudioEnd);
+    }, [playNext]);
+
+    useEffect(() => {
+        if (!audioRef.current || !currentSong) return;
+        const audio = audioRef.current;
+
+        // If source for previous audio doesn't match current audio's url therefore song has changed
+        const isSongChanged = prevAudioRef.current !== currentSong?.audioURL;
+
+        if (isSongChanged) {
+            audio.src = currentSong.audioURL;   // setting current audio's src
+            audio.currentTime = 0;   // resetting the duration
+
+            prevAudioRef.current = currentSong?.audioURL;   // setting prevAudioRef with current audio URL
+            if (isPlaying) audio.play();  // finally playing if isPlaying is true
+        }
+    }, [currentSong, isPlaying]);
+
+    return (
+        <audio ref={audioRef} />
+    )
+}
