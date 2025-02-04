6fc4033808ffa519fdf8b5e76660e8f259592b85
pg147 <xyzprathamesh147@gmail.com>
2025-02-04 23:07:23 +0530

    Responsive Changes for MainLayout - Conditional Rendering

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,3 +1,6 @@
+// React imports
+import { useEffect, useState } from "react";
+
 // Outlet from react-router-dom
 import { Outlet } from "react-router-dom";
 
@@ -5,35 +8,56 @@ import { Outlet } from "react-router-dom";
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 
 // Components
-import { FriendsActivity, LeftSideBar } from "@/components";
-import PlaybackControls from "@/components/PlaybackControls";
+import { FriendsActivity, LeftSideBar, PlaybackControls } from "@/components";
+import MobileNavbar from "@/components/MobileNavbar";
 
 export default function MainLayout() {
-  const isMobile = false;
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+
+    return () => window.removeEventListener("resize", checkMobile);
+  }, [])
 
   return (
     <div className="w-full h-screen flex flex-col">
-      <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow-hidden">
-        {/* Left Sidebar */}
-        <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={20}>
-          <LeftSideBar />
-        </ResizablePanel>
+      {isMobile && <MobileNavbar />}
 
-        {/* Control for resizing */}
-        <ResizableHandle />
-        
+      <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow-hidden">
+        {!isMobile && (
+          <>
+            {/* Left Sidebar */}
+            <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={20}>
+              <LeftSideBar />
+            </ResizablePanel>
+
+            {/* Control for resizing */}
+            <ResizableHandle />
+          </>
+        )}
+
         {/* Main Content */}
-        <ResizablePanel defaultSize={isMobile ? 80 : 60}>
+        <ResizablePanel defaultSize={isMobile ? 100 : 60}>
           <Outlet />
         </ResizablePanel>
 
-        {/* Control for resizing */}
-        <ResizableHandle />
+        {!isMobile && (
+          <>
+            {/* Control for resizing */}
+            <ResizableHandle />
 
-        {/* Friends Activity */}
-        <ResizablePanel defaultSize={20} minSize={0} maxSize={20} collapsedSize={0}> 
-          <FriendsActivity />
-        </ResizablePanel>
+            {/* Friends Activity */}
+            <ResizablePanel defaultSize={20} minSize={0} maxSize={20} collapsedSize={0}>
+              <FriendsActivity />
+            </ResizablePanel>
+          </>
+        )}
       </ResizablePanelGroup>
 
       <PlaybackControls />
