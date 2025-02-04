2adcd063d9b096c0c91422ea2ec34f1c9d4d1977
pg147 <xyzprathamesh147@gmail.com>
2025-02-05 00:20:43 +0530

    Favourites page created

--- frontend/src/pages/favourites/Favourites.tsx
+++ frontend/src/pages/favourites/Favourites.tsx
@@ -1,6 +1,12 @@
+import { InLoveIcon } from "hugeicons-react";
 
 export default function Favourites() {
   return (
-    <div>Favourites</div>
+    <div className="h-[calc(100vh-200px)] md:h-screen w-full flex items-center justify-center">
+      <div className="flex items-center gap-x-3.5">
+        <InLoveIcon className="size-6 md:size-8 text-primary" />
+        <h1 className="font-medium text-subheading">This feature will roll out soon!</h1>
+      </div>
+    </div>
   )
 }
