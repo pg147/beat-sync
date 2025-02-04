0e088da177ac9a52648095a968c8901a02071b46
pg147 <xyzprathamesh147@gmail.com>
2025-02-04 23:00:55 +0530

    Styling improvements - Brands

--- frontend/src/shared/Brand.tsx
+++ frontend/src/shared/Brand.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Brand() {
   return (
     <Link to={'/'} className="size-fit flex items-center gap-x-2">
-        <img src="/logo.png" alt="logo" className="size-8" />
+        <img src="/logo.png" alt="logo" className="size-7 md:size-8" />
         <h1 className="text-lg md:text-xl font-semibold">Beat<span className="text-primary">.sync</span></h1>
     </Link>
   )
