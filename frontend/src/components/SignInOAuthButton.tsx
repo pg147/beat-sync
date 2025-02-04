67097f0adf9a742541c25b1da99cc80f23429e98
pg147 <xyzprathamesh147@gmail.com>
2025-02-04 23:08:10 +0530

    Responsive changes for OAuthButton

--- frontend/src/components/SignInOAuthButton.tsx
+++ frontend/src/components/SignInOAuthButton.tsx
@@ -14,9 +14,9 @@ export default function SignInOAuthButton() {
   }
 
   return (
-    <div onClick={signInWithGoogle} className="size-fit cursor-pointer flex items-center gap-x-3 bg-tileLight lg:hover:bg-tileLight/60 rounded-xl px-6 py-3 font-medium transition-all duration-100 ease-in-out">
+    <div onClick={signInWithGoogle} className="size-fit cursor-pointer md:flex items-center gap-x-3 bg-tileLight lg:hover:bg-tileLight/60 rounded-xl px-2 py-2 md:px-6 md:py-3 font-medium transition-all duration-100 ease-in-out">
       <img src="/google-icon.svg" className="size-5" />
-      <p>Continue with Google </p>
+      <p className="hidden md:inline">Continue with Google </p>
     </div>
   )
 }
