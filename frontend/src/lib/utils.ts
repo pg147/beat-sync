10ff7270c3310a2d48e19bae6f2be5735c00d727
pg147 <xyzprathamesh147@gmail.com>
2025-02-04 19:52:14 +0530

    Floor function added

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export const formatDuration = (seconds: number) => {
   const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
+  const remainingSeconds = Math.floor(seconds % 60);
   return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
 };
 
