d51253eb05d1fc60935dc1024f2dc0e7e4a339b9
pg147 <xyzprathamesh147@gmail.com>
2025-02-04 20:24:02 +0530

    Queue functionality added at homepage

--- frontend/src/pages/homepage/Homepage.tsx
+++ frontend/src/pages/homepage/Homepage.tsx
@@ -1,9 +1,30 @@
+// React
+import { useEffect } from "react";
+
+// Components
 import { FeaturedSongs, MadeForYouSongs, Topbar, TrendingSongs } from "@/components";
+
+// Shadcn components
 import { Separator } from "@/components/ui/separator";
+
+// Global Store
+import { useMusicStore } from "@/store/useMusicStore";
+import { usePlayerStore } from "@/store/usePlayerStore";
+
+// Clerk Imports
 import { useUser } from "@clerk/clerk-react";
 
 export default function Homepage() {
   const { user } = useUser();
+  const { initializeQueue } = usePlayerStore();
+  const { featuredSongs, madeForYouSongs, trendingSongs } = useMusicStore();
+  
+  useEffect(() => {
+    if (featuredSongs.length > 0 || madeForYouSongs.length > 0 || trendingSongs.length > 0) {
+      const allSongs = [...featuredSongs, ...madeForYouSongs, ...trendingSongs];
+      initializeQueue(allSongs);
+    }
+  }, [featuredSongs, initializeQueue, madeForYouSongs, trendingSongs])
 
   return (
     <div className="h-screen w-full overflow-scroll">
