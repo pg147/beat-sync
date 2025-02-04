28676f2c649a4a5599e7f49388604193e5f305d9
pg147 <xyzprathamesh147@gmail.com>
2025-02-04 23:07:44 +0530

    Homepage optimized for mobile view

--- frontend/src/pages/homepage/Homepage.tsx
+++ frontend/src/pages/homepage/Homepage.tsx
@@ -18,7 +18,7 @@ export default function Homepage() {
   const { user } = useUser();
   const { initializeQueue } = usePlayerStore();
   const { featuredSongs, madeForYouSongs, trendingSongs } = useMusicStore();
-  
+
   useEffect(() => {
     if (featuredSongs.length > 0 || madeForYouSongs.length > 0 || trendingSongs.length > 0) {
       const allSongs = [...featuredSongs, ...madeForYouSongs, ...trendingSongs];
@@ -28,14 +28,14 @@ export default function Homepage() {
 
   return (
     <div className="h-screen w-full overflow-scroll">
-      <Topbar />
+      {window.innerWidth > 768 && <Topbar />}
 
-      {user && <div className="mt-7 w-full px-9">
-        <h1 className="text-xl font-semibold">Hi <span className="text-primary">{user?.firstName} !</span></h1>
-        <Separator className="my-6" />
+      {user && <div className="mt-5 md:mt-7 w-full md:px-9 px-5">
+        <h1 className="text-lg md:text-xl font-semibold">Hi <span className="text-primary">{user?.firstName} !</span></h1>
+        <Separator className="my-3 md:my-6" />
       </div>}
 
-      <div className="font-medium flex flex-col gap-y-6 mt-7 mb-36 w-full px-9">
+      <div className={`font-medium flex flex-col gap-y-4 md:gap-y-6 mt-5 w-full md:px-9 px-5 md:mt-7 mb-48 md:mb-36`}>
         <FeaturedSongs />
         <TrendingSongs />
         <MadeForYouSongs />
